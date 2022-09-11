2b05faa44d0e67ed1b8abbf83ecafa99c3286525
Gabridg <gabri99.del@gmail.com>
2022-09-11 16:13:21 +0200

    setted constructor & method

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
 <?php
+class Movie{
+    public $title;
+    public $year;
+    public $language;
+
+    public function __construct($title, $year, $language){
+        $this->$title = $title;
+        $this->$year = $year;
+        $this->$language= $language;
+
+    }
+
+    public function getFullInfo(){
+        return "$this->$title $this->$year $this->$language";
+    }
+}
+
+$movie1 = new Movie('Star Wars','2005','en' );
+$movie2 = new Movie('Il Signore degli Anelli','2003','en');
+
+?>
+
+
+
